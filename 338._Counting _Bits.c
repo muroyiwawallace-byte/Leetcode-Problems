9246f124a227330287f7008153f43ed31ca532cb
Muroyiwa Wallace <muroyiwawallace@gmail.com>
2026-01-25 10:22:01 +0530

    Create 338._Counting _Bits.c

---
+++ 338._Counting _Bits.c
@@ -0,0 +1,12 @@
+int* countBits(int n, int* returnSize) {
+    *returnSize = n + 1;
+
+    int* ans = (int*)malloc((n + 1) * sizeof(int));
+    ans[0] = 0;
+
+    for (int i = 1; i <= n; i++) {
+        ans[i] = ans[i / 2] + (i % 2);
+    }
+
+    return ans;
+}
