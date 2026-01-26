f6e937e7e66e98cbf0de916aaefce8d21d10b8cb
Muroyiwa Wallace <muroyiwawallace@gmail.com>
2026-01-26 09:53:38 +0530

    Create 292. _Nim Game.c

---
+++ 292. _Nim Game.c
@@ -0,0 +1,17 @@
+bool canWinNim(int n) {
+    if(n<=3){
+    return true;
+    }
+    else{
+        if(n%4==0){
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+
+
+    }    
+    
+}
