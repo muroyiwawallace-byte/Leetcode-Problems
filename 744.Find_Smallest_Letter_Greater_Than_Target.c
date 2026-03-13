7c46722e2b1df249b9c38886f0f187669cfd1c15
Muroyiwa Wallace <muroyiwawallace@gmail.com>
2026-03-13 12:10:16 +0530

    Create 744.Find_Smallest_Letter_Greater_Than_Target.c

---
+++ 744.Find_Smallest_Letter_Greater_Than_Target.c
@@ -0,0 +1,7 @@
+class Solution:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        for ch in letters:
+            if ch>target:
+                return ch
+                
+        return letters[0]
